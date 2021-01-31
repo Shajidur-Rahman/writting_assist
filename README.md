d2f1ea48fc49b0d46fb7b15aaba2c4832268efe5
Shajidur Rahman <shajidurrahmansaad10@gmail.com>
2021-01-31 19:29:20 +0600

    modifyed readme.file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # writting_assist
+#Hello I am Shajidur Rahman.
+#Here you can see 1 file. The file is main.py
+#Go to the file and copy past
+#remember you need to install python speech recognation
+
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import speech_recognition as sr
-from text_to_speech import speak 
 r = sr.Recognizer()
 while True:
     with sr.Microphone() as mic:
@@ -10,6 +9,6 @@ while True:
             print(reco)
         except sr.UnknownValueError:
             print("please speak again")
-            speak("please speak again", lang="en", save=True, file="hello.mp3")
         except sr.RequestError as p:
-           speak("hello", lang="en", save=True, file="hello.mp3")
+            print('')
+          
